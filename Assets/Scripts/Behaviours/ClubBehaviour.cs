8beee2a2e46ca3e4d0a3254a5542f637d863fd31
Chanisco Tromp <Chanisco@live.nl>
2020-07-11 21:36:17 +0200

    Club behaviour 81%

--- Assets/Scripts/Behaviours/ClubBehaviour.cs
+++ Assets/Scripts/Behaviours/ClubBehaviour.cs
@@ -5,56 +5,34 @@ using UnityEngine;
 public class ClubBehaviour : MonoBehaviour
 {
 
-    public CLUBPROGRESS ownProgress = CLUBPROGRESS.DETERMINE_POSITION;
     public GameObject ball, visual,head;
     public GameObject Grip;
 
-    public enum CLUBPROGRESS
-    {
-        DETERMINE_POSITION,
-        DETERMINE_POWER,
-        HIT,
-    }
 
     Vector3 StartPosition,ProgressPosition, EndPosition;
     Vector3 mOffset;
     float mZCoord;
     private void Start()
     {
-        Cursor.visible = false;
-        mZCoord = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
-        mOffset = Grip.transform.position - GetMouseWorldPos();
-        StartPosition = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+    //    Cursor.visible = false;
+
     }
 
     private void Update()
     {
-        if (ownProgress == CLUBPROGRESS.DETERMINE_POSITION)
+        if (Input.GetMouseButtonDown(0))
+        {
+            mZCoord = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
+            mOffset = Grip.transform.position - GetMouseWorldPos();
+        }
+        if (Input.GetMouseButton(0))
         {
             Vector3 targetPos = GetMouseWorldPos() + mOffset;
             targetPos.y = Grip.transform.position.y;
             Grip.transform.position = targetPos;
 
         }
-        else if(ownProgress == CLUBPROGRESS.DETERMINE_POWER)
-        {
-            if (Input.GetMouseButton(0))
-            {
-                ProgressPosition = Camera.main.ScreenToViewportPoint(Input.mousePosition);
-                float DifferenceX = ((StartPosition.x - ProgressPosition.x));
-                transform.RotateAround(this.transform.position, new Vector3(DifferenceX, 0, 0),1);
-            }
-            if (Input.GetMouseButtonUp(0))
-            {
-                StartPosition = Camera.main.ScreenToViewportPoint(Input.mousePosition);
-            }
 
-        }
-    }
-
-    private void OnMouseDown()
-    {
-        
     }
 
     Vector3 GetMouseWorldPos()
--- Assets/scripts/Behaviours/ClubHeadBehaviour.cs
+++ Assets/scripts/Behaviours/ClubHeadBehaviour.cs
@@ -8,27 +8,14 @@ public class ClubHeadBehaviour : MonoBehaviour
     private void OnCollisionEnter(Collision collision)
     {
         Rigidbody t = collision.gameObject.GetComponent<Rigidbody>();
-        Debug.Log("Work?");
         if (t != null)
         {
-            Debug.Log("Work = " + Vector3.Normalize(t.velocity) + " - " +CheckReceivedDirection(t.transform.position));
             Vector3 x = (Vector3.Normalize(t.velocity) + CheckReceivedDirection(t.transform.position)) * 1000;
             x.y = 0;
-            t.AddForce(x);//CheckReceivedDirection(collision.transform.position) * 1050);
+            t.AddForce(x);
         }
 
 
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        Rigidbody t = other.gameObject.GetComponent<Rigidbody>();
-        Debug.Log("blues?");
-        if (t != null)
-        {
-            Debug.Log("blues");
-            t.AddRelativeForce(Vector3.forward * 1050);
-        }
     }
 
     private Vector3 CheckReceivedDirection(Vector3 targetPosition)
