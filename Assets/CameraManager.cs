c4cb009ad618313ba123f1c2cab6cf255ff47e55
DESKTOP-I3RJQLK\Aaron <aaronligthart@hotmail.com>
2020-07-11 20:55:42 +0200

    Made smooth camera movement and transitions

---
+++ Assets/CameraManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    public GameManager gameManager;
+    private Camera camera;
+
+    public float minDistanceToFocus;
+    public float maxDistanceToFocus;
+    public float minDistance;
+    public float maxDistance;
+    public float minZoom;
+    public float maxZoom;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameManager.distanceToHole >= minDistance && gameManager.distanceToHole <= maxDistance)
+        {
+            this.UpdateZoom(gameManager.distanceToHole);
+        }  else if (gameManager.distanceToHole > maxDistance) 
+        {
+            Camera.main.fieldOfView = minZoom;
+        }
+
+        if (gameManager.distanceToHole >= minDistanceToFocus && gameManager.distanceToHole <= maxDistanceToFocus)
+        {
+            this.UpdateFocus(gameManager.distanceToHole);
+        }
+        else if (gameManager.distanceToHole > maxDistanceToFocus)
+        {
+            gameManager.focusPriority = 0;
+        };
+    }
+
+    public void UpdateZoom(float distance)
+    {
+        float diffA = maxDistance - minDistance;
+        float diffB = maxZoom - minZoom;
+        float step = diffA / diffB;
+
+        Camera.main.fieldOfView = distance * step + minZoom;
+    }
+    public void UpdateFocus(float distance)
+    {
+        float diffA = maxDistanceToFocus - minDistanceToFocus;
+        float diffB = 1;
+        float step = diffB / diffA;
+ 
+
+        gameManager.focusPriority = 1 - (distance * step);
+
+    }
+}
--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -8,11 +8,15 @@ public class GameManager : MonoBehaviour
     public GameObject ball;
     public GameObject spawnPositon;
     public GameObject hole;
+    public Transform cameraFocusPoint;
+
     private Rigidbody ballRigidbody;
     public TextMeshProUGUI instructionText;
     public TextMeshProUGUI distanceText;
     public TextMeshProUGUI windStrengthText;
     public float distanceToHole;
+    public float focusPriority;
+
 
 
 
@@ -27,10 +31,30 @@ public class GameManager : MonoBehaviour
         this.UpdateDistanceText();
     }
 
-    public void UpdateDistanceText(){
-        distanceToHole = (Mathf.Round(Vector3.Distance(ball.transform.position, hole.transform.position) * 10f) / 10f)-1;
+    public void UpdateDistanceText()
+    {
+        distanceToHole = (Mathf.Round(Vector3.Distance(ball.transform.position, hole.transform.position) * 10f) / 10f) - 1;
         distanceText.SetText(distanceToHole + " m");
     }
+
+    public void FixedUpdate()
+    {
+        this.updateFocusPoint();
+
+    }
+
+    public void updateFocusPoint()
+    {
+        Vector3 pointA = this.ball.transform.position; // Position of objectA
+        Vector3 pointB = this.hole.transform.position; // Position of objectB
+        Vector3 pointC = Vector3.Lerp(pointA, pointB, focusPriority);
+
+        this.cameraFocusPoint.position = Vector3.Lerp(this.cameraFocusPoint.position, pointC, 0.15f) ;
+
+        distanceToHole = (Mathf.Round(Vector3.Distance(ball.transform.position, hole.transform.position) * 10f) / 10f) - 1;
+        distanceText.SetText(distanceToHole + " m");
+    }
+
     public void ResetBall()
     {
         ball.GetComponent<Ball>().Reset();
