2074f29e1c54d8b6c0e25bcdcb95cc964decf77f
DESKTOP-I3RJQLK\Aaron <aaronligthart@hotmail.com>
2020-07-11 21:33:14 +0200

    Started on spawning levels

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    public List<GameObject> levels;
+    public int currentLevelIndex;
+    public GameObject currentLevel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.currentLevel = levels[currentLevelIndex];
+        this.SpawnLevel(this.currentLevel);
+    }
+
+    // Update is called once per frame
+    public void gotoNextLevel()
+    {
+        GameObject nextLevel = levels[this.currentLevelIndex];
+        this.currentLevel.SetActive(false);
+        SpawnLevel(nextLevel);
+        GameObject.Destroy(this.currentLevel);
+        this.currentLevel = nextLevel;
+    }
+
+    public void SpawnLevel(GameObject level)
+    {
+        Instantiate(level, new Vector3(0, 0, 0), Quaternion.identity);
+    }
+}
