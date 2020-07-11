dce3b34a1fd1db253ecae9d3f184377a439e4195
DESKTOP-I3RJQLK\Aaron <aaronligthart@hotmail.com>
2020-07-12 01:48:03 +0200

    added a pachinco screen

---
+++ Assets/FollowPachincoBall.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPachincoBall : MonoBehaviour
+{
+    public GameObject ball;
+    public float offset;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 pointA = this.ball.transform.position; // Position of objectA
+        Vector3 pointB = this.transform.position; // Position of objectB
+        Vector3 pointC = Vector3.Lerp(pointA, pointB, offset);
+
+        this.transform.position = Vector3.Lerp(this.transform.position, pointC, 0.15f);
+        this.transform.position = new Vector3(0, this.transform.position.y, -9);
+
+    }
+}
---
+++ Assets/PachincoManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PachincoManager : MonoBehaviour
+{
+    public GameObject ball;
+    public GameObject spawn;
+    public Camera pachincoCamera;
+    public float radius;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ball.transform.SetPositionAndRotation(getRandomStartPosition(), Quaternion.identity);
+    }
+
+    public Vector3 getRandomStartPosition()
+    {
+        float randomX = Random.Range(spawn.transform.position.x - radius, spawn.transform.position.x + radius);
+        float randomY = Random.Range(spawn.transform.position.y - radius, spawn.transform.position.y + radius);
+
+        Vector3 pos = new Vector3(randomX, randomY, spawn.transform.position.z);
+        return pos;
+    }
+
+    // Update is called once per frame
+}
