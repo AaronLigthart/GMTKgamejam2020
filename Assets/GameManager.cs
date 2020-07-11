257629ee796099dac18fc2c677a891aca096814c
DESKTOP-I3RJQLK\Aaron <aaronligthart@hotmail.com>
2020-07-11 15:25:58 +0200

    added a hole object and made the ball reset on put;.

---
+++ Assets/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject ball;
+    public GameObject spawnPositon;
+    private Rigidbody ballRigidbody; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        ballRigidbody = ball.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ResetBall()
+    {
+        var spawnPosition = new Vector3(spawnPositon.transform.position.x, spawnPositon.transform.position.y + ball.transform.localScale.y / 2, spawnPositon.transform.position.z);
+        ball.transform.SetPositionAndRotation(spawnPosition, spawnPositon.transform.rotation);
+        ballRigidbody.velocity = Vector3.zero;
+        ballRigidbody.angularVelocity = Vector3.zero;
+
+    }
+}
---
+++ Assets/Hole.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Events;
+using UnityEngine;
+
+public class Hole : MonoBehaviour
+{
+    public UnityEvent onPut;
+
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider other)
+    {
+        print(other.tag);
+        onPut.Invoke();
+    }
+    
+        
+    
+}
